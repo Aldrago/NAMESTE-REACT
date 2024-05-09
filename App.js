50477813d86c6333501651144eb77f077a8d2095
Aldrago <pranshujb007@gmail.com>
2024-05-09 21:08:09 +0530

    e1

---
+++ App.js
@@ -0,0 +1,15 @@
+const header = React.createElement("div", {id:'header'}, "Header");
+
+const parent = React.createElement("div", {id:'parent'}, 
+                                    [React.createElement("div", {id:'child'},
+                                        [React.createElement("h1", {id:'h1-tag'}, "hello from react"),
+                                        React.createElement("h2", {id: "h2-tag"}, "h2 tag")]),
+                                    React.createElement("div", {id:"child2"}, 
+                                        [React.createElement("h1", {id:'h1-tag2'}, "hello from react"),
+                                        React.createElement("h2", {id: "h2-tag2"}, "h2 tag")])]);
+const footer = React.createElement("div", {id:'footer'}, "Footer");
+const mainContainer = React.createElement("div", {id: 'main-container'}, [header, parent, footer]);
+console.log(mainContainer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(mainContainer);
