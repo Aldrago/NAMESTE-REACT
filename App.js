fcbd25bf962dafdb550acde78b9288c6bcc24f94
Aldrago <pranshujb007@gmail.com>
2024-05-11 15:03:42 +0530

    e3

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const header = React.createElement("div", {id:'header'}, "Header");
+const Title = () => <h1 id="title">Title of the document</h1>;
 
-const parent = React.createElement("div", {id:'parent'}, 
-                                    [React.createElement("div", {id:'child'},
-                                        [React.createElement("h1", {id:'h1-tag'}, "hello from react"),
-                                        React.createElement("h2", {id: "h2-tag"}, "h2 tag")]),
-                                    React.createElement("div", {id:"child2"}, 
-                                        [React.createElement("h1", {id:'h1-tag2'}, "hello from react"),
-                                        React.createElement("h2", {id: "h2-tag2"}, "h2 tag")])]);
-const footer = React.createElement("div", {id:'footer'}, "Footer");
-const mainContainer = React.createElement("div", {id: 'main-container'}, [header, parent, footer]);
-console.log(mainContainer);
+const Heading = () => <h1 id="heading">Heading of the document</h1>;
+
+const Page = () => (
+  <div id="container">
+    <Title />
+    <Heading />
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(mainContainer);
+root.render(<Page />);
